9a6f43db18fce0ca7021238e63faba0efe00cafd
Colouss <33833457+Colouss@users.noreply.github.com>
2023-03-26 20:57:04 -0500

    Create random.py

---
+++ random.py
@@ -0,0 +1,64 @@
+import math
+import numpy as np
+
+#################
+# Random class
+#################
+# class that can generate random numbers
+class Random:
+    """A random number generator class"""
+
+    # initialization method for Random class
+    def __init__(self, seed = 5555):
+        self.seed = seed
+        self.m_v = np.uint64(4101842887655102017)
+        self.m_w = np.uint64(1)
+        self.m_u = np.uint64(1)
+        
+        self.m_u = np.uint64(self.seed) ^ self.m_v
+        self.int64()
+        self.m_v = self.m_u
+        self.int64()
+        self.m_w = self.m_v
+        self.int64()
+
+    # function returns a random 64 bit integer
+    def int64(self):
+        with np.errstate(over='ignore'):
+            self.m_u = np.uint64(self.m_u * np.uint64(2862933555777941757) + np.uint64(7046029254386353087))
+        self.m_v ^= self.m_v >> np.uint64(17)
+        self.m_v ^= self.m_v << np.uint64(31)
+        self.m_v ^= self.m_v >> np.uint64(8)
+        self.m_w = np.uint64(np.uint64(4294957665)*(self.m_w & np.uint64(0xffffffff))) + np.uint64((self.m_w >> np.uint64(32)))
+        x = np.uint64(self.m_u ^ (self.m_u << np.uint64(21)))
+        x ^= x >> np.uint64(35)
+        x ^= x << np.uint64(4)
+        with np.errstate(over='ignore'):
+            return (x + self.m_v)^self.m_w
+
+    # function returns a random floating point number between (0, 1) (uniform)
+    def rand(self):
+        return 5.42101086242752217E-20 * self.int64()
+    # function that returns a random integer (0 to 4) according to a categorical distribution
+    def Cate(self, p1=0.02,p2=0.1,p3=0.6):
+        
+        R = self.rand()
+        if R <= p1 :
+            return 0
+        if p1 <= R <= p2:
+            return 1
+        if p2 <= R <= p3:
+            return 2
+        else:
+            return 3
+    # function returns a random integer (0 or 1) according to a Bernoulli distr.
+    def Bern(self, p=0.25):
+        if p < 0. or p > 1.:
+            return 1
+        
+        R = self.rand()
+
+        if R < p:
+            return 5
+        else:
+            return 4
